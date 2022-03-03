3e28dfb35dcfd59a5e5e3f3697f859fe2285d25a
ZifengChen <ken36chan@gmail.com>
2022-03-03 14:20:58 +0800

    chore: 添加ts类型定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,40 @@
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import React, { useEffect, useState } from "react";
+import { cleanObject, useMount, useDebounce } from "utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [params, setParams] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParams = useDebounce(params, 200);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParams))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParams]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+  return (
+    <div>
+      <SearchPanel users={users} param={params} setParams={setParams} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
---
+++ src/screens/project-list/search-panel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParams: (param: SearchPanelProps["param"]) => void;
+}
+export const SearchPanel = ({ users, param, setParams }: SearchPanelProps) => {
+  return (
+    <form action="">
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) => setParams({ ...param, name: evt.target.value })}
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParams({ ...param, personId: evt.target.value })
+          }
+        >
+          <option value="">负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
