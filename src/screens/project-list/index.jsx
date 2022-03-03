3eeffe8327938d71f19b6f5771d39a713c023963
ZifengChen <ken36chan@gmail.com>
2022-03-03 11:10:00 +0800

    feat: 添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
 import { React, useEffect,useState } from "react"
-import { cleanObject } from "utils"
+import { cleanObject, useMount, useDebounce } from "utils"
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -11,23 +11,24 @@ export const ProjectListScreen = () => {
     name:'',
     personId: ''
   })
+  const debouncedParams = useDebounce(params, 2000)
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(params))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParams))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  }, [params])
+  }, [debouncedParams])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  },[])
+  })
   return <div>
     <SearchPanel users={users} params={params} setParams={setParams}/>
     <List users={users} list={list}/>
