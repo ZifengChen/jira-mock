2036b1f19a6b9ed51c0cdeed899bbf7f2cd9b43d
ZifengChen <ken36chan@gmail.com>
2022-11-05 00:22:42 +0800

    feat: iterator

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -8,24 +8,28 @@ import styled from "@emotion/styled";
 import { useProjects } from "utils/project";
 import { useUser } from "utils/user";
 import { Typography } from "antd";
+import { useUrlQueryParam } from "utils/url";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
-  const [params, setParams] = useState({
+  const [, setParam] = useState({
     name: "",
     personId: "",
   });
-  const debouncedParams = useDebounce(params, 200);
+  const [param] = useUrlQueryParam(["name", "personId"]);
+
+  const debouncedParams = useDebounce(param, 200);
 
   useDocumentTitle("项目列表", false);
 
   const { isLoading, error, data: list } = useProjects(debouncedParams);
 
   const { data: users } = useUser(debouncedParams);
+
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users || []} param={params} setParams={setParams} />
+      <SearchPanel users={users || []} param={param} setParams={setParam} />
       {error ? (
         <Typography.Text type="danger">{error.message}</Typography.Text>
       ) : null}
@@ -33,6 +37,8 @@ export const ProjectListScreen = () => {
     </Container>
   );
 };
+
+ProjectListScreen.whyDidYouRender = true;
 
 const Container = styled.div`
   padding: 3.2rem;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,7 +19,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <D>(value: D, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebouncedValue(value), delay);
